91c57836a463a810b91066d548bddd1f08518984
NikhilAgrawal-07 <nikhil.agrawal1_cs19@gla.ac.in>
2022-06-01 23:19:43 +0530

    Intro

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>MINI-PROJECT</h1>
+        <h2>(2021-22)</h2>
+        <h1>KICKIN</h1>
+        <h1>PROJECT REPORT</h1>
+        <img src="./logo.png"></img>
+        <h1>Institute of Engineering AND Technology </h1>
+        <h1>Submitted By:</h1>
+        <h1>Nikhil Agarwal (B) 191500494</h1>
+        <h1>Under the supervision of</h1>
+        <h1> Mr. Mandeep Singh </h1>
+        <h1> (Technical Trainer)</h1>
+        <h1> Department Of Computer Engineering AND Technology</h1>
       </header>
     </div>
   );
